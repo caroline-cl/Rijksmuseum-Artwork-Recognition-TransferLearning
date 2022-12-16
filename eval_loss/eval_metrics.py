4361d2ca88225bc92a8963a9302fb63da9094dc2
Krismxhe <114190087+Krismxhe@users.noreply.github.com>
2022-12-15 22:40:23 -0500

    Create eval_metrics.py

---
+++ eval_loss/eval_metrics.py
@@ -0,0 +1,42 @@
+import seaborn as sns
+from sklearn.metrics import confusion_matrix,f1_score,recall_score,precision_score,balanced_accuracy_score
+
+def eval_all(predict_list, truth_label):
+    #creator
+    acc=0
+    for index in range(len(predict_list[0])):
+      acc = acc + accuracy_score(truth_label[0][index], predict_list[0][index])
+    acc = acc / len(predict_list[0])
+    f1_c=f1_score(truth_label[0],predict_list[0],average='micro')
+    recal_c = recall_score(truth_label[0],predict_list[0],average='micro')
+    prec_c = precision_score(truth_label[0],predict_list[0],average='micro')
+    mac_c = balanced_accuracy_score(truth_label[0],predict_list[0])
+    con_mat_c=confusion_matrix(truth_label[0],predict_list[0])
+
+    #material
+    mmAp = 0
+    for index in range(len(predict_list[1])):
+      mmAp = mmAp + average_precision_score(truth_label[1][index], predict_list[1][index], average='weighted')
+    mmAp = mmAp / len(predict_list[1])
+
+    mmRecall = 0
+    for index in range(len(predict_list[1])):
+      mmRecall = mmRecall + recall_score(truth_label[1][index], predict_list[1][index], average='weighted')
+    mmRecall = mmRecall/len(predict_list[1])
+
+    mf1 = 2*(mmAp*mmRecall)/(mmAp+mmRecall)
+
+    #type
+    tmAp = 0
+    for index in range(len(predict_list[2])):
+      tmAp = tmAp + average_precision_score(truth_label[2][index], predict_list[2][index], average='weighted')
+    tmAp = tmAp / len(predict_list[2])
+
+    tmRecall = 0
+    for index in range(len(predict_list[2])):
+      tmRecall = tmRecall + recall_score(truth_label[2][index], predict_list[2][index], average='weighted')
+    tmRecall = tmRecall/len(predict_list[2])
+
+    tf1 = 2*(tmAp*tmRecall)/(tmAp+tmRecall)
+
+    return [f1_c,recal_c,prec_c,prec_c,mac_c,con_mat_c,acc], [mmAp,mmRecall,mf1], [tmAp,tmRecall,tf1]
