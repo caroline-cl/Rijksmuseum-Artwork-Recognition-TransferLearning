eb7c21c6125b081b97b3a5d6ee54735f2ba68f72
Krismxhe <114190087+Krismxhe@users.noreply.github.com>
2022-12-15 22:48:52 -0500

    Update train_loss.py

--- eval_loss/train_loss.py
+++ eval_loss/train_loss.py
@@ -1,6 +1,33 @@
 ###main train and evaluate function ###
 from tqdm import tqdm
 import time
+import os
+import pickle
+from collections import OrderedDict
+import torch
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+import torch.nn as nn
+import cv2
+import random
+import torchvision.transforms as transforms
+from torch.utils import data
+from torch.nn import functional as F
+from torchvision import models
+import matplotlib.pyplot as plt
+import pandas as pd
+import torchvision
+import numpy as np
+import torch.optim as optim
+import torchvision.datasets as td
+from torchvision import transforms
+from PIL import Image
+from skimage.feature import hog as hog
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import plot_confusion_matrix,f1_score, accuracy_score, average_precision_score
+from sklearn.preprocessing import OneHotEncoder, MultiLabelBinarizer, LabelEncoder
+
 
 def train_main(model,loss_,optimizer,train_loader,device, epoch):
     loss_epoch=[]
